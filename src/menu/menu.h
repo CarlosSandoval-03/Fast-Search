1d121bf89ed8e7a89605ce0bc3bbc61ea96a70e0
Carlos Sandoval <csandovalc@unal.edu.co>
2023-04-15 00:39:16 -0500

    feat(docs): add menu module

--- src/menu/menu.h
+++ src/menu/menu.h
@@ -1,6 +1,14 @@
+/******************************************************************************************
+ * Copyright (C) 2023 by Carlos Sandoval                                                  *
+ *                                                                                        *
+ * This file is part of Fast-Search.                                                      *
+ * @author Carlos Santiago Sandoval Casallas, https://github.com/CarlosSandoval-03        *
+ * Released under the terms of the MIT license, see: https://opensource.org/license/mit/  *
+ ******************************************************************************************/
 #ifndef _MENU_H_
 #define _MENU_H_
 
+// Option-Related Macros
 #define SRCID_OPTION 1
 #define DSTID_OPTION 2
 #define HOD_OPTION 3
