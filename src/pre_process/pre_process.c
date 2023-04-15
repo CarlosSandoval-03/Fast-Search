69b269d215afd7043c067cc8bc1a170be844fa1a
Carlos Sandoval <csandovalc@unal.edu.co>
2023-04-15 00:55:11 -0500

    feat(docs): add pre process module

--- src/pre_process/pre_process.c
+++ src/pre_process/pre_process.c
@@ -1,11 +1,18 @@
+/******************************************************************************************
+ * Copyright (C) 2023 by Carlos Sandoval                                                  *
+ *                                                                                        *
+ * This file is part of Fast-Search.                                                      *
+ * @author Carlos Santiago Sandoval Casallas, https://github.com/CarlosSandoval-03        *
+ * Released under the terms of the MIT license, see: https://opensource.org/license/mit/  *
+ ******************************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "../file/file.h"
 #include "./pre_process.h"
 #include "../structures/structures.h"
 
+// Raw info
 typedef struct {
 	unsigned short srcid;
 	unsigned short dstid;
@@ -13,6 +20,13 @@ typedef struct {
 	float mean_travel_time;
 } raw_t;
 
+/**
+ * @brief Creates a new instance of raw_t struct.
+ * @return raw_t* A pointer to the newly allocated memory for raw_t struct.
+ * @note This function allocates memory for a new instance of the raw_t struct using the malloc function.
+ * If the allocation fails, the function exits the program with an error message.
+ * The function then returns a pointer to the newly allocated memory for raw_t struct.
+ */
 raw_t *new_raw()
 {
 	raw_t *raw = (raw_t *)malloc(sizeof(raw_t));
@@ -24,6 +38,11 @@ raw_t *new_raw()
 	return raw;
 }
 
+/**
+ * @brief Clears the contents of a char buffer
+ * @param buffer The buffer to clear
+ * @param size_buff The size of the buffer
+ */
 void clear_buffer(char *buffer, int size_buff)
 {
 	for (int i = 0; i < size_buff; i++) {
@@ -31,6 +50,13 @@ void clear_buffer(char *buffer, int size_buff)
 	}
 }
 
+/**
+ * @brief Reads data from a CSV file, storing it in a buffer.
+ * @param fp The file pointer to the CSV file to read from
+ * @param buffer The buffer where the data will be stored
+ * @param size_buff The size of the buffer
+ * @return A pointer to the buffer containing the data, or NULL if the end of file has been reached.
+ */
 const char *get_data(FILE *fp, char *buffer, int size_buff)
 {
 	if (feof(fp)) {
@@ -60,6 +86,11 @@ const char *get_data(FILE *fp, char *buffer, int size_buff)
 	return buffer;
 }
 
+/**
+ * @brief Parse data from a buffer in CSV format and return a new raw_t struct.
+ * @param buffer A pointer to the buffer containing the CSV-formatted data.
+ * @return A pointer to a new raw_t struct containing the parsed data.
+ */
 raw_t *parse_data(char *buffer)
 {
 	raw_t *raw = new_raw();
@@ -90,6 +121,12 @@ raw_t *parse_data(char *buffer)
 	return raw;
 }
 
+/**
+ * @brief Process a line of CSV-formatted data by parsing it into a raw_t struct and inserting it into the given hash table.
+ * @param buffer A pointer to the buffer containing the CSV-formatted data to be processed.
+ * @param ptr_hash A pointer to the hash table where the parsed data will be inserted.
+ * @return void
+ */
 void process_line(char *buffer, hash_t *ptr_hash)
 {
 	if (buffer == NULL) {
--- src/pre_process/pre_process.h
+++ src/pre_process/pre_process.h
@@ -1,12 +1,21 @@
+/******************************************************************************************
+ * Copyright (C) 2023 by Carlos Sandoval                                                  *
+ *                                                                                        *
+ * This file is part of Fast-Search.                                                      *
+ * @author Carlos Santiago Sandoval Casallas, https://github.com/CarlosSandoval-03        *
+ * Released under the terms of the MIT license, see: https://opensource.org/license/mit/  *
+ ******************************************************************************************/
 #ifndef _PRE_PROCESS_H_
 #define _PRE_PROCESS_H_
 
 #include "../structures/structures.h"
 
+// Default file location
 #define DEFAULT_PATH_DATA "data/raw/uber_data.csv"
 #define DEFAULT_PATH_LIST "data/processed/list.bin"
 #define DEFAULT_PATH_HASH "data/processed/hash.bin"
 
+// Default CSV Features
 #define DELIMITER_CSV_STRING ","
 #define DELIMITER_CSV_CHAR ','
 #define NUM_PARAMS_TO_TAKE 4
